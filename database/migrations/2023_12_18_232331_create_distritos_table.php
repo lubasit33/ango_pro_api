0a6639cc1ceb569070118f636775e47f0013d4c7
Lubas IT 33 <lubasit33@gmail.com>
2023-12-19 00:30:08 +0100

    feat: criação e definição das migrations

---
+++ database/migrations/2023_12_18_232331_create_distritos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistritosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distritos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_distrito', 21);
+            $table->foreignId('municipio_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distritos');
+    }
+
+}
