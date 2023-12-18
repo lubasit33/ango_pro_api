d01f55c243a78db20475815f8143037fbc1f6a24
Lubas IT 33 <lubasit33@gmail.com>
2023-12-19 00:37:52 +0100

    feat: definição dos relacionamentos entre os modelos

--- app/Models/Municipio.php
+++ app/Models/Municipio.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Municipio extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'nome_municipio', 'provincia_id',
+    ];
+
+    public function provincia()
+    {
+        return $this->belongsTo(Provincia::class);
+    }
+
+    public function comunas()
+    {
+        return $this->hasMany(Comuna::class);
+    }
+
+    public function distritos()
+    {
+        return $this->hasMany(Distrito::class);
+    }
+
 }
--- app/Models/Provincia.php
+++ app/Models/Provincia.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Provincia extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'nome_provincia',
+    ];
+
+    public function municipios()
+    {
+        return $this->hasMany(Municipio::class);
+    }
+
 }
